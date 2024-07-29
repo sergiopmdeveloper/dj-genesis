6d03ae25dfa06806407041e84bbb86fecb7a9f88
Sergio <sergio.pm.developer@gmail.com>
2024-07-29 23:18:50 +0000

    feat: validate sign in form fields and implement form errors template

--- auth/views.py
+++ auth/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.views.decorators.http import require_http_methods
 from django_htmx.http import HttpResponseClientRefresh
 
 
+@require_http_methods(["GET", "POST"])
 def sign_in(request: HttpRequest) -> HttpResponse:
     """
     The sign in view
@@ -18,7 +20,21 @@ def sign_in(request: HttpRequest) -> HttpResponse:
         The rendered sign in page
     """
 
-    if request.htmx:
+    if request.method == "POST" and request.htmx:
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+
+        errors = []
+
+        if not email:
+            errors.append("Email is required")
+
+        if not password:
+            errors.append("Password is required")
+
+        if errors:
+            return render(request, "form-errors.html", {"errors": errors})
+
         return HttpResponseClientRefresh()
 
     return render(request, "auth/sign-in.html")
