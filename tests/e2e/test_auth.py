3e60698500cf7623be6a7e2c3f27cfa6caf90cf3
Sergio <sergio.pm.developer@gmail.com>
2024-08-02 17:35:41 +0000

    test: implement e2e tests of the sign up view

--- tests/e2e/test_auth.py
+++ tests/e2e/test_auth.py
@@ -82,3 +82,91 @@ def test_sign_in_post_success(client: Client):
 
     assert response.status_code == 200
     assert client.session["_auth_user_id"] == str(user.id)
+
+
+def test_sign_up_get_success(client: Client):
+    """
+    Tests the GET method of the sign up view and expects
+    the response to be successful and the correct templates to be used
+    """
+
+    response = client.get(reverse("sign-up"))
+    templates = [template.name for template in response.templates]
+
+    assert response.status_code == 200
+    assert "page.html" in templates
+    assert "auth/sign-up.html" in templates
+
+
+@pytest.mark.django_db
+def test_sign_up_get_user_authenticated(client: Client):
+    """
+    Tests the GET method of the sign up view when the user is authenticated
+    and expects the response to be a redirection
+    """
+
+    user = User.objects.create_user(username="username", password="password")
+
+    client.force_login(user=user)
+    response = client.get(reverse("sign-up"))
+
+    assert response.status_code == 302
+
+
+def test_sign_up_post_invalid_data(client: Client):
+    """
+    Tests the POST method of the sign up view with invalid data
+    and expects the response to be successful and the correct templates to be used
+    """
+
+    response = client.post(reverse("sign-up"), data={}, HTTP_HX_REQUEST="true")
+    templates = [template.name for template in response.templates]
+
+    assert response.status_code == 200
+    assert "form-errors.html" in templates
+
+
+@pytest.mark.django_db
+def test_sign_up_post_invalid_user(client: Client):
+    """
+    Tests the POST method of the sign up view with invalid user
+    and expects the response to be successful and the correct templates to be used
+    """
+
+    User.objects.create_user(username="username")
+
+    response = client.post(
+        reverse("sign-up"),
+        data={
+            "username": "username",
+            "email": "username@email.com",
+            "password": "password152",
+        },
+        HTTP_HX_REQUEST="true",
+    )
+
+    templates = [template.name for template in response.templates]
+
+    assert response.status_code == 200
+    assert "form-errors.html" in templates
+
+
+@pytest.mark.django_db
+def test_sign_up_post_success(client: Client):
+    """
+    Tests the POST method of the sign up view with valid data
+    and expects the response to be successful and the user to be authenticated
+    """
+
+    response = client.post(
+        reverse("sign-up"),
+        data={
+            "username": "username",
+            "email": "username@email.com",
+            "password": "password152",
+        },
+        HTTP_HX_REQUEST="true",
+    )
+
+    assert response.status_code == 200
+    assert client.session["_auth_user_id"]
